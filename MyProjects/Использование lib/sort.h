4fc4b6c1913ec72693f09a099c773ceca60fbb4a
Rbaim <radmir200@mail.ru>
2017-10-10 20:22:51 +0300

    [private]

--- MyProjects/Использование lib/sort.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void SortBubble(int *array, int size);
-
--- R.baim/LR_R.Baim/14/func.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-struct Matrix {
-	double **mass;
-	int n;
-	int m;
-};
-
-double ColumnArithmetic(Matrix &A, int num);
